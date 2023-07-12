955a9e6b7efcf1a754a236dfe0b77fb6b7865c60
Nick <darkforceyoda@gmail.com>
2023-07-11 23:16:04 -0500

    updated imports for Customizer

--- src/pages/Customizer.tsx
+++ src/pages/Customizer.tsx
@@ -1,4 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+import config from "valtio";
+import state from "../store";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
 
 type CustomizerProps = {};
 
