b71a9df993bca99d78dae5852d656afb207a9727
Nick <darkforceyoda@gmail.com>
2023-07-11 23:15:15 -0500

    padding

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 function App() {
 
   return (
-    <main className='transition-all ease-in'>
+    <main className='transition-all ease-in px-24 py-12'>
       <h1 className='text-4xl'>ThreeJS</h1>
       <Home />
       <Canvas />
