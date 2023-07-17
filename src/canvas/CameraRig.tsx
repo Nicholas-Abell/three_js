d354b43e75e9c2463f7b95fd241cfd48740fe31a
Nick <darkforceyoda@gmail.com>
2023-07-16 21:49:50 -0500

    cameraRig imports

--- src/canvas/CameraRig.tsx
+++ src/canvas/CameraRig.tsx
@@ -1,10 +1,15 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import state from "../store";
 
 type CameraRigProps = {
   children: ReactNode;
 };
 
 const CameraRig: React.FC<CameraRigProps> = ({ children }) => {
-  return <div>{children}</div>;
+  const group = useRef();
+  return <group>{children}</group>;
 };
 export default CameraRig;
--- src/canvas/Shirt.tsx
+++ src/canvas/Shirt.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { useFrame } from "@react-three/fiber";
+import { Decal, useGLTF, useTexture } from "@react-three/drei";
+import state from "../store";
 
-type ShirtProps = {
-    
+type ShirtProps = {};
+
+const Shirt: React.FC<ShirtProps> = () => {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF("/shirt_baked.glb");
+
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = snap.fullDecal;
+
+  return (
+    <group>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      ></mesh>
+    </group>
+  );
 };
-
-const Shirt:React.FC<ShirtProps> = () => {
-    
-    return <div>Have a good coding</div>
-}
 export default Shirt;
